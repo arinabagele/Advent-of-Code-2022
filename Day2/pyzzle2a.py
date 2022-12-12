c1bc0c74b3ac67600b79ae59345ab79855a12a4c
Arina Bagele <61218417+arinabagele@users.noreply.github.com>
2022-12-12 11:40:07 +1100

    Update pyzzle2a.py

--- Day2/pyzzle2a.py
+++ Day2/pyzzle2a.py
@@ -1,1 +1,31 @@
 
+result = 0
+with open('puzzle2_input.txt', 'r') as file:
+    # Rock:     A   X (1)
+    # Paper:    B   Y (2)
+    # Scissors: C   Z (3)
+    #won: 6
+    #lost: 0
+    #draw: 3
+    for line in file:
+        l = line.strip()
+        
+        if l[-1] == 'X':
+            result += 1
+        elif l[-1] == 'Y':
+            result += 2
+        else:
+            result += 3
+        
+        # winning combinations
+        if l in ['A Y', 'C X', 'B Z']:
+            result += 6
+
+        # draw combinations
+        elif l in ['A X', 'B Y', 'C Z']:
+            result += 3
+
+        else:
+            result += 0
+
+print(result)
